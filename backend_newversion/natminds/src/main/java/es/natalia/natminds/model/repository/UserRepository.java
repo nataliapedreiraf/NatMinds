37d0e4514bc1daad96d527347dbeeef49bf61da2
nataliapedreiraf <102594090+nataliapedreiraf@users.noreply.github.com>
2023-11-11 18:40:52 +0100

    Nueva versión del proyecto
    Debido al tiempo que nos queda debemos simplificar, así que subo nueva versión del proyecto.

---
+++ backend_newversion/natminds/src/main/java/es/natalia/natminds/model/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package es.natalia.natminds.model.repository;
+
+import es.natalia.natminds.model.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+public interface UserRepository
+        extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {}
---
+++ backend_newversion/natminds/src/test/java/es/natalia/natminds/NatmindsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.natalia.natminds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatmindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
